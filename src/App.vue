31370d4654c9b2d3317a732b3b74aab670560ad7
crazy0023 <124178927@qq.com>
2017-11-17 09:06:37 +0800

    ~

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,23 @@
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
-@import './assets/less/main';
+@import "./assets/less/main";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  img{
-    filter: sepia(120)
+  img {
+    filter: sepia(120);
   }
-  .transitionLine{
+  .transitionLine {
     width: 0;
     height: 2px;
     background-color: transparent;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -49,60 +49,64 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      checkList: [1,5,3],
-      tableData6: [{
-          id: '12987122',
-          name: '王小虎',
-          amount1: '234',
-          amount2: '3.2',
+      checkList: [1, 5, 3],
+      tableData6: [
+        {
+          id: "12987122",
+          name: "王小虎",
+          amount1: "234",
+          amount2: "3.2",
           amount3: 10
-        }, {
-          id: '12987123',
-          name: '王小虎',
-          amount1: '165',
-          amount2: '4.43',
+        },
+        {
+          id: "12987123",
+          name: "王小虎",
+          amount1: "165",
+          amount2: "4.43",
           amount3: 12
-        }, {
-          id: '12987124',
-          name: '王小虎',
-          amount1: '324',
-          amount2: '1.9',
+        },
+        {
+          id: "12987124",
+          name: "王小虎",
+          amount1: "324",
+          amount2: "1.9",
           amount3: 9
-        }, {
-          id: '12987125',
-          name: '王小虎',
-          amount1: '621',
-          amount2: '2.2',
+        },
+        {
+          id: "12987125",
+          name: "王小虎",
+          amount1: "621",
+          amount2: "2.2",
           amount3: 17
-        }, {
-          id: '12987126',
-          name: '王小虎',
-          amount1: '539',
-          amount2: '4.1',
+        },
+        {
+          id: "12987126",
+          name: "王小虎",
+          amount1: "539",
+          amount2: "4.1",
           amount3: 15
-        }]
-    }
+        }
+      ]
+    };
   },
   methods: {
     indexMethod(index) {
-      return index+1
+      return index + 1;
     },
-    ren (h,{column,$index}){
-      return h('div',{},column.label)
+    ren(h, { column, $index }) {
+      return h("div", {}, column.label);
     },
-    ran(h,{column,$index}) {
-      console.log($index)
+    ran(h, { column, $index }) {
+      console.log($index);
     }
-    
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 
 </style>
